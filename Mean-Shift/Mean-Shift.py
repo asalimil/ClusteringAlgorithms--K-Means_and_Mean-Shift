67d3eabeaba27033713732da2ecb472eb6e08d1e
asalimil <43479233+asalimil@users.noreply.github.com>
2021-04-24 18:46:54 -0700

    Update Mean-Shift.py

--- Mean-Shift/Mean-Shift.py
+++ Mean-Shift/Mean-Shift.py
@@ -2,7 +2,7 @@ from numpy import unique
 from numpy import where
 from sklearn.datasets import make_classification
 from sklearn.cluster import MeanShift
-# from matplotlib import pyplot
+from matplotlib.pyplot import plt
 # define dataset
 X, _ = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=4)
 # define the model
